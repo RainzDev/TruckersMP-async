ef97093d3a93b7ff8b1e41cf29c8277229a482a0
rainzDev <62964686+RainzDev@users.noreply.github.com>
2024-03-20 18:46:15 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+VERSION = "0.0.1"  # PEP-440
+
+NAME = "TruckersMP.py"
+
+INSTALL_REQUIRES = [
+    "aiohttp",
+]
+
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    description="An asynchronous TruckersMP API wrapper for grabbing data.",
+    url="https://github.com/RainzDev/TruckersMP.py/",
+    project_urls={
+        "Source Code": "https://github.com/RainzDev/TruckersMP.py/",
+    },
+    author="Jess",
+    author_email="jessrblx16@gmail.com",
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.10",
+    ],
+    # Snowpark requires Python 3.8
+    python_requires=">=3.8",
+    # Requirements
+    install_requires=INSTALL_REQUIRES,
+    packages=["streamlit_gsheets"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
